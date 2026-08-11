bd2cd7631403ca9ac137314080af85488daf28d4
aeson000 <71472221+aeson000@users.noreply.github.com>
2026-08-11 14:27:05 -0400

    timer update - edit to code

--- templates.config.js
+++ templates.config.js
@@ -1,295 +1,260 @@
 window.FBX_TEMPLATES = [
-    {
-      name: "E-100",
-      modelFile: "model_1.fbx",
-      templateFile: "template_all.png",
-
-      body: {
-        baseColorMap: "E_100_BC.png",
-        baseNormalMap: "E_100_NM.png",
-        baseRMMap: "E_100_RM.png",
-        miscMap: "E_100_MISC.png",
-        maskMap: "E_100_MASK.png"
-      },
-
-      defaultTiles: 5,
-
-      viewCode: {
-  "position": [
-    0,
-    0,
-    0
-  ],
-  "rotation": [
-    0,
-    0.017453,
-    0
-  ],
-  "scale": 1,
-  "modelRotationY": 1,
-  "focusPoint": [
-    318.748482,
-    -21.075182,
-    124.309581
-  ],
-  "viewDirection": [
-    0.611959,
-    0.230779,
-    -0.75647
-  ],
-  "framingRadius": 496.259868,
-  "cameraPosition": [
-    1125.755633,
-    283.259427,
-    -873.268197
-  ],
-  "cameraTarget": [
-    318.748482,
-    -21.075182,
-    124.309581
-  ],
-  "fov": 38,
-  "near": 607.583894,
-  "far": 2086.742729,
-  "modelVerticalOffset": 2,
-  "modelHorizontalOffset": -36,
-  "viewportAspect": 1.777778
-}
+  {
+    "name": "E-100",
+    "modelFile": "model_1.fbx",
+    "templateFile": "template_all.png",
+    "body": {
+      "baseColorMap": "E_100_BC.png",
+      "baseNormalMap": "E_100_NM.png",
+      "baseRMMap": "E_100_RM.png",
+      "miscMap": "E_100_MISC.png",
+      "maskMap": "E_100_MASK.png"
     },
-	    {
-      name: "IS-7",
-      modelFile: "model_2.fbx",
-      templateFile: "template_all.png",
-
-      body: {
-    baseColorMap: "IS-7_BC.dx11.png",
-    baseNormalMap: "IS-7_NM.dx11.png",
-    baseRMMap: "IS-7_RM.dx11.png",
-    miscMap: "IS-7_MISC.dx11.png",
-    maskMap: "IS-7_MASK.dx11.png"
-  },
-
-      defaultTiles: 5,
-
-      viewCode: {
-  "position": [
-    0,
-    0,
-    0
-  ],
-  "rotation": [
-    0,
-    0.069813,
-    0
-  ],
-  "scale": 1,
-  "modelRotationY": 4,
-  "focusPoint": [
-    0,
-    167.796828,
-    -75.927452
-  ],
-  "viewDirection": [
-    0.616742,
-    0.164986,
-    -0.769681
-  ],
-  "framingRadius": 421.45469,
-  "fov": 39,
-  "near": 133.870719,
-  "far": 1964.851365,
-  "modelVerticalOffset": 20,
-  "modelHorizontalOffset": -15,
-  "viewportAspect": 1.781302
-}
-    },
-	    {
-      name: "BZT-70",
-      modelFile: "model_3A.fbx",
-      templateFile: "template_all.png",
-
-      body: {
-    baseColorMap: "Ch57_BZT_70_BC.dx11.png",
-    baseNormalMap: "Ch57_BZT_70_NM.dx11.png",
-    baseRMMap: "Ch57_BZT_70_RM.dx11.png",
-    miscMap: "Ch57_BZT_70_MISC.dx11.png",
-    maskMap: "Ch57_BZT_70_MASK.dx11.png"
-  },
-
-      defaultTiles: 5,
-
-      viewCode: {
-  "position": [
-    0,
-    0,
-    0
-  ],
-  "rotation": [
-    0,
-    0.069813,
-    0
-  ],
-  "scale": 1,
-  "modelRotationY": 4,
-  "focusPoint": [
-    0,
-    167.796828,
-    -75.927452
-  ],
-  "viewDirection": [
-    0.596393,
-    0.130215,
-    -0.79206
-  ],
-  "framingRadius": 455.978588,
-  "fov": 40,
-  "near": 224.7977,
-  "far": 2017.673075,
-  "modelVerticalOffset": 16.5,
-  "modelHorizontalOffset": -15,
-  "viewportAspect": 1.781302
-}
-    },
-	    {
-      name: "Carro 45t",
-      modelFile: "model_4.fbx",
-      templateFile: "template_all.png",
-
-      body: {
-    baseColorMap: "It20_Car_Comb_45t_BC.dx11.png",
-    baseNormalMap: "It20_Car_Comb_45t_NM.dx11.png",
-    baseRMMap: "It20_Car_Comb_45t_RM.dx11.png",
-    miscMap: "It20_Car_Comb_45t_MISC.dx11.png",
-    maskMap: "It20_Car_Comb_45t_MASK.dx11.png"
-  },
-
-      defaultTiles: 5,
-
-      viewCode: {
-  "position": [
-    0,
-    0,
-    0
-  ],
-  "rotation": [
-    0,
-    0.069813,
-    0
-  ],
-  "scale": 1,
-  "modelRotationY": 4,
-  "focusPoint": [
-    0,
-    167.796828,
-    -75.927452
-  ],
-  "viewDirection": [
-    -0.504238,
-    0.194629,
-    -0.841346
-  ],
-  "framingRadius": 455.978588,
-  "fov": 40,
-  "near": 419.768823,
-  "far": 1896.398378,
-  "modelVerticalOffset": 16.5,
-  "modelHorizontalOffset": -15,
-  "viewportAspect": 1.781302
-}
-    },
-	    {
-      name: "Kpz 70",
-      modelFile: "model_5.fbx",
-      templateFile: "template_all.png",
-
-      body: {
-    baseNormalMap: "KpfPz_70_NM.dx11.png",
-    baseColorMap: "G_KpfPz_70_BC.dx11.png",
-    baseRMMap: "G_KpfPz_70_RM.dx11.png",
-    miscMap: "G_KpfPz_70_MISC.dx11.png",
-    maskMap: "G_KpfPz_70_MASK.dx11.png"
-  },
-
-      defaultTiles: 5,
-
-      viewCode: {
-  "position": [
-    0,
-    0,
-    0
-  ],
-  "rotation": [
-    0,
-    0.122173,
-    0
-  ],
-  "scale": 1,
-  "modelRotationY": 7,
-  "focusPoint": [
-    0,
-    167.796828,
-    -75.927452
-  ],
-  "viewDirection": [
-    -0.570044,
-    0.148783,
-    -0.808031
-  ],
-  "framingRadius": 475.823131,
-  "fov": 50,
-  "near": 357.953334,
-  "far": 1662.542589,
-  "modelVerticalOffset": 22,
-  "modelHorizontalOffset": -16.5,
-  "viewportAspect": 1.781302
-}
-    },
-	    {
-      name: "VK 90.01 (P)",
-      modelFile: "model_6.fbx",
-      templateFile: "template_all.png",
-
-      body: {
-    baseColorMap: "G_VK_9001P_BC.dx11.png",
-    baseNormalMap: "G_VK_9001P_NM.dx11.png",
-    baseRMMap: "G_VK_9001P_RM.dx11.png",
-    miscMap: "G_VK_9001P_MISC.dx11.png",
-    maskMap: "G_VK_9001P_MASK.dx11.png"
-  },
-
-      defaultTiles: 5,
-
-      viewCode: {
-  "position": [
-    0,
-    0,
-    0
-  ],
-  "rotation": [
-    0,
-    0.122173,
-    0
-  ],
-  "scale": 1,
-  "modelRotationY": 7,
-  "focusPoint": [
-    0,
-    167.796828,
-    -75.927452
-  ],
-  "viewDirection": [
-    -0.570044,
-    0.148783,
-    -0.808031
-  ],
-  "framingRadius": 475.823131,
-  "fov": 50,
-  "near": 357.953334,
-  "far": 1662.542589,
-  "modelVerticalOffset": 22,
-  "modelHorizontalOffset": -16.5,
-  "viewportAspect": 1.781302
-}
+    "defaultTiles": 5,
+    "viewCode": {
+      "position": [
+        0,
+        0,
+        0
+      ],
+      "rotation": [
+        0,
+        0.017453,
+        0
+      ],
+      "scale": 1,
+      "modelRotationY": 1,
+      "focusPoint": [
+        318.748482,
+        -21.075182,
+        124.309581
+      ],
+      "viewDirection": [
+        0.611959,
+        0.230779,
+        -0.75647
+      ],
+      "framingRadius": 496.259868,
+      "fov": 38,
+      "near": 607.583894,
+      "far": 2086.742729,
+      "modelVerticalOffset": 2,
+      "modelHorizontalOffset": -36
     }
-	
-  ];
+  },
+  {
+    "name": "IS-7",
+    "modelFile": "model_2.fbx",
+    "templateFile": "template_all.png",
+    "body": {
+      "baseColorMap": "IS-7_BC.dx11.png",
+      "baseNormalMap": "IS-7_NM.dx11.png",
+      "baseRMMap": "IS-7_RM.dx11.png",
+      "miscMap": "IS-7_MISC.dx11.png",
+      "maskMap": "IS-7_MASK.dx11.png"
+    },
+    "defaultTiles": 5,
+    "viewCode": {
+      "position": [
+        0,
+        0,
+        0
+      ],
+      "rotation": [
+        0,
+        0.069813,
+        0
+      ],
+      "scale": 1,
+      "modelRotationY": 4,
+      "focusPoint": [
+        0,
+        167.796828,
+        -75.927452
+      ],
+      "viewDirection": [
+        0.616742,
+        0.164986,
+        -0.769681
+      ],
+      "framingRadius": 421.45469,
+      "fov": 39,
+      "near": 133.870719,
+      "far": 1964.851365,
+      "modelVerticalOffset": 20,
+      "modelHorizontalOffset": -15
+    }
+  },
+  {
+    "name": "BZT-70",
+    "modelFile": "model_3A.fbx",
+    "templateFile": "template_all.png",
+    "body": {
+      "baseColorMap": "Ch57_BZT_70_BC.dx11.png",
+      "baseNormalMap": "Ch57_BZT_70_NM.dx11.png",
+      "baseRMMap": "Ch57_BZT_70_RM.dx11.png",
+      "miscMap": "Ch57_BZT_70_MISC.dx11.png",
+      "maskMap": "Ch57_BZT_70_MASK.dx11.png"
+    },
+    "defaultTiles": 5,
+    "viewCode": {
+      "position": [
+        0,
+        0,
+        0
+      ],
+      "rotation": [
+        0,
+        0.069813,
+        0
+      ],
+      "scale": 1,
+      "modelRotationY": 4,
+      "focusPoint": [
+        0,
+        167.796828,
+        -75.927452
+      ],
+      "viewDirection": [
+        0.596393,
+        0.130215,
+        -0.79206
+      ],
+      "framingRadius": 455.978588,
+      "fov": 40,
+      "near": 224.7977,
+      "far": 2017.673075,
+      "modelVerticalOffset": 16.5,
+      "modelHorizontalOffset": -15
+    }
+  },
+  {
+    "name": "Carro 45t",
+    "modelFile": "model_4.fbx",
+    "templateFile": "template_all.png",
+    "body": {
+      "baseColorMap": "It20_Car_Comb_45t_BC.dx11.png",
+      "baseNormalMap": "It20_Car_Comb_45t_NM.dx11.png",
+      "baseRMMap": "It20_Car_Comb_45t_RM.dx11.png",
+      "miscMap": "It20_Car_Comb_45t_MISC.dx11.png",
+      "maskMap": "It20_Car_Comb_45t_MASK.dx11.png"
+    },
+    "defaultTiles": 5,
+    "viewCode": {
+      "position": [
+        0,
+        0,
+        0
+      ],
+      "rotation": [
+        0,
+        0.069813,
+        0
+      ],
+      "scale": 1,
+      "modelRotationY": 4,
+      "focusPoint": [
+        0,
+        167.796828,
+        -75.927452
+      ],
+      "viewDirection": [
+        -0.504238,
+        0.194629,
+        -0.841346
+      ],
+      "framingRadius": 455.978588,
+      "fov": 40,
+      "near": 419.768823,
+      "far": 1896.398378,
+      "modelVerticalOffset": 16.5,
+      "modelHorizontalOffset": -15
+    }
+  },
+  {
+    "name": "Kpz 70",
+    "modelFile": "model_5.fbx",
+    "templateFile": "template_all.png",
+    "body": {
+      "baseNormalMap": "KpfPz_70_NM.dx11.png",
+      "baseColorMap": "G_KpfPz_70_BC.dx11.png",
+      "baseRMMap": "G_KpfPz_70_RM.dx11.png",
+      "miscMap": "G_KpfPz_70_MISC.dx11.png",
+      "maskMap": "G_KpfPz_70_MASK.dx11.png"
+    },
+    "defaultTiles": 5,
+    "viewCode": {
+      "position": [
+        0,
+        0,
+        0
+      ],
+      "rotation": [
+        0,
+        0.122173,
+        0
+      ],
+      "scale": 1,
+      "modelRotationY": 7,
+      "focusPoint": [
+        0,
+        167.796828,
+        -75.927452
+      ],
+      "viewDirection": [
+        -0.570044,
+        0.148783,
+        -0.808031
+      ],
+      "framingRadius": 475.823131,
+      "fov": 50,
+      "near": 357.953334,
+      "far": 1662.542589,
+      "modelVerticalOffset": 22,
+      "modelHorizontalOffset": -16.5
+    }
+  },
+  {
+    "name": "VK 90.01 (P)",
+    "modelFile": "model_6.fbx",
+    "templateFile": "template_all.png",
+    "body": {
+      "baseColorMap": "G_VK_9001P_BC.dx11.png",
+      "baseNormalMap": "G_VK_9001P_NM.dx11.png",
+      "baseRMMap": "G_VK_9001P_RM.dx11.png",
+      "miscMap": "G_VK_9001P_MISC.dx11.png",
+      "maskMap": "G_VK_9001P_MASK.dx11.png"
+    },
+    "defaultTiles": 5,
+    "viewCode": {
+      "position": [
+        0,
+        0,
+        0
+      ],
+      "rotation": [
+        0,
+        0.122173,
+        0
+      ],
+      "scale": 1,
+      "modelRotationY": 7,
+      "focusPoint": [
+        0,
+        167.796828,
+        -75.927452
+      ],
+      "viewDirection": [
+        -0.570044,
+        0.148783,
+        -0.808031
+      ],
+      "framingRadius": 475.823131,
+      "fov": 50,
+      "near": 357.953334,
+      "far": 1662.542589,
+      "modelVerticalOffset": 22,
+      "modelHorizontalOffset": -16.5
+    }
+  }
+];
